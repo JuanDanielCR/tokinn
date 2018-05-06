24d5612073e2b3873d537133f2881224dc8c392c
JuanDanielCR <castilloreyesjuan@gmail.com>
2018-05-06 17:50:41 -0500

    Commit inicial

---
+++ src/main/java/com/escom/tokinn/TokinnApplication.java
@@ -0,0 +1,12 @@
+package com.escom.tokinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokinnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokinnApplication.class, args);
+	}
+}
