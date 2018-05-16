4df4295ed3e9b0b6455c4bde0326ec4a76cbb63a
JuanDanielCR <castilloreyesjuan@gmail.com>
2018-05-15 22:59:36 -0500

    Envio tokens chatbot v1

--- src/main/java/com/escom/tokinn/controller/MessengerController.java
+++ src/main/java/com/escom/tokinn/controller/MessengerController.java
@@ -33,6 +33,10 @@ import com.github.messenger4j.send.buttons.Button;
 import com.github.messenger4j.send.templates.ButtonTemplate;
 import com.github.messenger4j.send.templates.GenericTemplate;
 import com.github.messenger4j.send.templates.ReceiptTemplate;
+
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -54,9 +58,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessengerController {
 	private static final String RESOURCE_URL = "https://raw.githubusercontent.com/fbsamples/messenger-platform-samples/master/node/public";
 	private static final Log logger = LogFactory.getLog(MessengerController.class);
-	
     private final MessengerReceiveClient receiveClient;
     private final MessengerSendClient sendClient;
+    
+    //TODO: Pasar esto a constantes negocio
+    private static final String INICIO = "INICIO";
+    private static final String ESTADO = "ESTADO_DE_CUENTA";
+    private static final String TRANSACCION = "TRANSACCION";
+    private final int tokenLength=5;
 
     /**
      * Constructs the {@code MessengerPlatformCallbackHandler} and initializes the {@code MessengerReceiveClient}.
@@ -68,11 +77,9 @@ public class MessengerController {
      */
     @Autowired
     public MessengerController(@Value("${messenger4j.appSecret}") final String appSecret,
-                                            @Value("${messenger4j.verifyToken}") final String verifyToken,
-                                            final MessengerSendClient sendClient) {
-
+                               @Value("${messenger4j.verifyToken}") final String verifyToken,
+                               final MessengerSendClient sendClient) {
         logger.debug("Initializing MessengerReceiveClient - appSecret: "+appSecret+" | verifyToken: "+ verifyToken);
-        
         this.receiveClient = MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
                 .onTextMessageEvent(newTextMessageEventHandler())
                 .onAttachmentMessageEvent(newAttachmentMessageEventHandler())
@@ -126,6 +133,78 @@ public class MessengerController {
         }
     }
 
+    //TODO: Pasar esto aun service ----------------------------------------------------
+    private static String getHash(String txt) {
+        try {
+            MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(txt.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                //Linea para homogeneizar el hash producido al convertirlo en un String
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    
+    /*Funcion que regresa una cadena con el formato requerido de la fecha y hora*/
+    private String getDateTime(Date date){
+        SimpleDateFormat formato = new SimpleDateFormat("yyyyMMddhhmm");
+        return formato.format(date);
+    }
+    
+    /*Funcion que suma o resta minutos a la fecha actual*/
+    private Date addMinutes(Date date, int num){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE, num);
+        return calendar.getTime();
+    }
+    
+    /*Funcion que genera los tokens si se restan o suman minutos*/
+    private String generateToken(String id, String type, Date date){
+        return getHash(id+type+getDateTime(date)).substring(0, tokenLength).toUpperCase();
+    }
+    
+    /*Funcion que genera el token actual ESTA FUNCION ES LA QUE USA EL BOT
+    Se manda el ID del usuario y se instancia el TIPO DE TOKEN
+    devuelve una cadena con el TOKEN GENERADO*/
+    public String generateToken(String id, String type){
+        return getHash(id+type+getDateTime(new Date())).substring(0, tokenLength).toUpperCase();
+    }
+    
+    /*Funcion que verifica el token, tiene un umbral de 2 a 3 minutos ESTA FUNCION VA EN EL SERVIDOR
+    Se manda el ID del usuario, se instancia el TIPO DE TOKEN y se manda el TOKEN RECIVIDO
+    devuelve TRUE si es aceptado y un FALSE si no*/
+    public boolean verifyToken(String id, String type, String token){
+        if(token.equals(generateToken(id,type,new Date()))){
+            return true;
+        }else if(token.equals(generateToken(id,type,addMinutes(new Date(),-2)))){
+            return true;
+        }else if(token.equals(generateToken(id,type,addMinutes(new Date(),-1)))){
+            return true;
+        }
+        return false;
+    } 
+    
+    //--------------------------------
+    private void sendTokenMessage(String recipientId, String text, String tipoToken) {
+        try {
+        	//TODO:Revisar si el ChatBot esta habilitado
+            final Recipient recipient = Recipient.newBuilder().recipientId(recipientId).build();
+            final NotificationType notificationType = NotificationType.REGULAR;
+            final String metadata = "DEVELOPER_DEFINED_METADATA";
+            
+            this.sendClient.sendTextMessage(recipient, notificationType, generateToken(recipientId, tipoToken), metadata);
+            
+        } catch (MessengerApiException | MessengerIOException e) {
+            handleSendException(e);
+        }
+    }
+    
     private TextMessageEventHandler newTextMessageEventHandler() {
         return event -> {
             logger.debug("Received TextMessageEvent: "+ event);
@@ -135,10 +214,21 @@ public class MessengerController {
             final String senderId = event.getSender().getId();
             final Date timestamp = event.getTimestamp();
 
+            System.out.println("senderId: "+senderId);
+            System.out.println("messageText: "+messageText);
             logger.info("Received message '"+messageId+"' with text '"+messageText+"' from user '"+senderId+"' at '"+timestamp+"'");
 
             try {
                 switch (messageText.toLowerCase()) {
+	                case "token_inicio":
+	                    sendTokenMessage(senderId, messageText, INICIO);
+	                    break;
+	                case "token_estado":
+	                    sendTokenMessage(senderId, messageText, ESTADO);
+	                    break;
+	                case "token_transaccion":
+	                    sendTokenMessage(senderId, messageText, TRANSACCION);
+	                    break;
                     case "image":
                         sendImageMessage(senderId);
                         break;
@@ -186,12 +276,6 @@ public class MessengerController {
                     case "typing off":
                         sendTypingOff(senderId);
                         break;
-
-                    /*
-                    case "account linking":
-                        sendAccountLinking(senderId);
-                        break;
-                    */
 
                     default:
                         sendTextMessage(senderId, messageText);
@@ -320,11 +404,6 @@ public class MessengerController {
 
     private void sendTypingOff(String recipientId) throws MessengerApiException, MessengerIOException {
         this.sendClient.sendSenderAction(recipientId, SenderAction.TYPING_OFF);
-    }
-
-    private void sendAccountLinking(String recipientId) {
-        // supported by messenger4j since 0.7.0
-        // sample implementation coming soon
     }
 
     private AttachmentMessageEventHandler newAttachmentMessageEventHandler() {
@@ -474,7 +553,7 @@ public class MessengerController {
             final Recipient recipient = Recipient.newBuilder().recipientId(recipientId).build();
             final NotificationType notificationType = NotificationType.REGULAR;
             final String metadata = "DEVELOPER_DEFINED_METADATA";
-
+            
             this.sendClient.sendTextMessage(recipient, notificationType, text, metadata);
         } catch (MessengerApiException | MessengerIOException e) {
             handleSendException(e);
