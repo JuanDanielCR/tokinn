71569d0d981ea6795bad80635a3725b84e254578
JuanDanielCR <castilloreyesjuan@gmail.com>
2018-05-27 08:23:09 -0500

    Mapeos y servicios

--- src/main/java/com/escom/tokinn/constantes/Constantes.java
+++
@@ -1,7 +0,0 @@
-package com.escom.tokinn.constantes;
-
-public class Constantes {
-	//Constantes de navegacion
-	public static final String LANDING_VIEW = "landing";
-	public static final String TARJETA_VIEW = "login_tarjeta";
-}
