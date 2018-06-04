f3931f966471a9bf18e257f987a3956c6059151e
Ana Paola Nava Vivas <ananava1996@gmail.com>
2018-06-03 23:51:31 -0500

    Add files via upload

--- src/main/java/com/escom/tokinn/controller/CuentaController.java
+++ src/main/java/com/escom/tokinn/controller/CuentaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.escom.tokinn.constantes.NavigationConstants;
+import com.escom.tokinn.constantes.Constantes;
 import com.escom.tokinn.services.CuentaService;
 
 @Controller
@@ -19,16 +19,21 @@ public class CuentaController {
 	
 	@GetMapping("/registro")
 	public ModelAndView registrar() {
-		return new ModelAndView(NavigationConstants.CUENTA_ADD);
+		return new ModelAndView(Constantes.CUENTA_ADD);
 	}
 	
 	@GetMapping("/gestion")
 	public ModelAndView gestionar() {
-		return new ModelAndView(NavigationConstants.CUENTA_INDEX);
+		return new ModelAndView(Constantes.CUENTA_INDEX);
 	}
+
+	@GetMapping("/pagos")
+	public ModelAndView pagos() {
+		return new ModelAndView(Constantes.CUENTA_PAGOS);
+	}	
 	
 	@GetMapping("/test")
 	public ModelAndView test() {
-		return new ModelAndView(NavigationConstants.CUENTA_VULNERABILIDAD);
+		return new ModelAndView(Constantes.CUENTA_VULNERABILIDAD);
 	}
 }
